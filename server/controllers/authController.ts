1ce82ed461c625c1d1f3f361c7d2909a131ff4e0
Aran Hayer <ashayer@users.noreply.github.com>
2022-07-28 18:35:39 -0700

    basic error auth handling in backend

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,6 +1,23 @@
 import express from "express";
 import User from "../models/userModel.js";
 
+const handleError = (err: any) => {
+  let errors: any = { email: "", password: "" };
+
+  if (err.code === 11000) {
+    errors.email = "Account already exists"
+    return errors
+  };
+
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }: any) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
 export const signUp = async (req: express.Request, res: express.Response) => {
   const { email, password } = req.body;
 
@@ -8,8 +25,8 @@ export const signUp = async (req: express.Request, res: express.Response) => {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
-    console.error(err);
-    res.status(400).send("error bozo");
+    const errors = handleError(err);
+    res.status(400).json({ errors });
   }
 };
 
--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema(
   {
     // auth data to store
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minLength: 6 },
+    email: {
+      type: String,
+      required: [true, "Email Required"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Invalid Email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password Required "],
+      minlength: [6, "Minimum password length is 6"],
+    },
   },
   {
     collection: "users",
