cefda4667ec4ba1175839e1fccfd0b8011f7b438
Aran Hayer <ashayer@users.noreply.github.com>
2022-07-30 23:38:53 -0700

    exported errorHandler function to separate file

---
+++ server/middleware/errorHandler.ts
@@ -0,0 +1,32 @@
+export const handleError = (err: any) => {
+  let errors: any = { email: "", password: "" };
+
+  if (err.code === 11000) {
+    errors.email = "Account already exists";
+    return errors;
+  }
+
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }: any) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  if (err.message.includes("Account does not exist with that email")) {
+    errors.email = "Account does not exist with that email";
+  }
+
+  if (err.message.includes("Invalid password")) {
+    errors.password = "Invalid password";
+  }
+
+  if (err.message.includes("Username Required")) {
+    errors.username = "Username required";
+  }
+
+  if (err.message.includes("Invalid Username")) {
+    errors.username = "Username must be letters and numbers only";
+  }
+
+  return errors;
+};
