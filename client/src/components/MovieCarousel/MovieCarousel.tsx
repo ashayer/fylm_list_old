88e050ff01d92f0fa7e35e00f33bbc95a3a5df0c
Aran Hayer <ashayer@users.noreply.github.com>
2022-07-31 22:25:37 -0700

    added movie carousel component

--- client/src/components/MovieCarousel/MovieCarousel.tsx
+++ client/src/components/MovieCarousel/MovieCarousel.tsx
@@ -1,8 +1,59 @@
-import { Box, Button, Card, CardMedia, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useState } from "react";
 
-const MovieCarousel = () => {
-  return <Box></Box>;
+const MovieCarousel = ({ movieList }: { movieList: MoviePopular[] }) => {
+  const [index, setIndex] = useState(0);
+  return (
+    <Grid container sx={{ backgroundColor: "white", mt: "1rem", justifyContent: "center" }}>
+      <Grid item>
+        <Typography variant="h2">Trending Now</Typography>
+      </Grid>
+      <Grid container sx={{ justifyContent: "center" }}>
+        <Grid item>
+          <Button
+            variant="contained"
+            onClick={() => {
+              index > 0 ? setIndex(index - 1) : setIndex(movieList.length - 1);
+            }}
+            sx={{ height: "50vh" }}
+          >
+            Prev
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <Box
+            sx={{
+              backgroundImage: `url(https://image.tmdb.org/t/p/original/${movieList[index].backdrop_path})`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              border: "1px solid white",
+              color: "white",
+              mb: 1,
+            }}
+            height="50vh"
+            width="50vw"
+          >
+            <Typography>{movieList[index].title}</Typography>
+            <Typography>{movieList[index].overview}</Typography>
+          </Box>
+        </Grid>
+
+        <Grid item>
+          <Button
+            variant="contained"
+            onClick={() => {
+              index < movieList.length - 1 ? setIndex(index + 1) : setIndex(0);
+            }}
+            sx={{ height: "50vh" }}
+          >
+            Next
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default MovieCarousel;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,28 +1,37 @@
-import { Button } from "@mui/material";
-import React, { useEffect } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { type } from "@testing-library/user-event/dist/type";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import MovieCarousel from "../../components/MovieCarousel/MovieCarousel";
 import useStore from "../../store";
+
+const getPopularMovies = async () => {
+  try {
+    const response = await axios.get("/api/movie/getPopular");
+    return response.data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const Home = () => {
   const navigate = useNavigate();
-
   const user = useStore((state) => state.isUser);
-  const setUser = useStore((state) => state.setIsUser);
-
-  const onLogout = () => {
-    setUser(false);
-    navigate("/login");
-  };
-
+  const [movieList, setMovieList] = useState<MoviePopular[]>([]);
   useEffect(() => {
     if (!user) navigate("/login");
-  });
+    else {
+      getPopularMovies().then((movies) => {
+        setMovieList(movies);
+      });
+    }
+  }, [navigate, user]);
+
   return (
-    <div>
-      <Button onClick={() => onLogout()} variant="contained">
-        LOGOUT
-      </Button>
-    </div>
+    <Box>
+      <Box>{movieList.length > 0 && <MovieCarousel movieList={movieList} />}</Box>
+    </Box>
   );
 };
 
