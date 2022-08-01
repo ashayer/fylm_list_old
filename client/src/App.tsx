e360ffdddfcb9ec11d93d4974c4c9e9cd64aa4f9
Aran Hayer <ashayer@users.noreply.github.com>
2022-07-31 22:24:35 -0700

    removed global html background

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,17 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import "@fontsource/manrope";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/movie/:movieId" element={<MovieDetails />} />
         <Route path="*" element={<h1>404 not found</h1>} />
       </Routes>
@@ -24,4 +25,3 @@ function App() {
 }
 
 export default App;
-
