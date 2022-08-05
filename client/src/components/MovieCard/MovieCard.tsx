16b2428473bec1acf265787f9034f4060dd8cfa6
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-05 02:27:38 -0700

    added zoom in transition on movie cards

--- client/src/components/MovieCard/MovieCard.tsx
+++ client/src/components/MovieCard/MovieCard.tsx
@@ -1,30 +1,32 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Zoom } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import styles from "./movieCardStyles";
 
 const MovieCard = ({ movieDetails }: { movieDetails: MoviePopular }) => {
   const navigate = useNavigate();
   return (
-    <Grid
-      container
-      item
-      sx={{ ...styles.movieCardContainer }}
-      onClick={() => navigate(`/movie/${movieDetails.id}`)}
-    >
+    <Zoom in>
       <Grid
+        container
         item
-        sx={{
-          ...styles.movieCardBackground,
-          backgroundImage: `url(https://image.tmdb.org/t/p/w200${movieDetails.poster_path})`,
-        }}
-      />
-      <Typography sx={{ ...styles.movieCardText }}>
-        <strong>{movieDetails.title}</strong>
-      </Typography>
-      <Typography sx={{ ...styles.movieCardText }}>
-        <strong>{movieDetails.release_date}</strong>
-      </Typography>
-    </Grid>
+        sx={{ ...styles.movieCardContainer }}
+        onClick={() => navigate(`/movie/${movieDetails.id}`)}
+      >
+        <Grid
+          item
+          sx={{
+            ...styles.movieCardBackground,
+            backgroundImage: `url(https://image.tmdb.org/t/p/w200${movieDetails.poster_path})`,
+          }}
+        />
+        <Typography sx={{ ...styles.movieCardText }}>
+          <strong>{movieDetails.title}</strong>
+        </Typography>
+        <Typography sx={{ ...styles.movieCardText }}>
+          <strong>{movieDetails.release_date}</strong>
+        </Typography>
+      </Grid>
+    </Zoom>
   );
 };
 
