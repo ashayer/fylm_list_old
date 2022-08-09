da2cee2d3825932b2c7ee0d4f1201f1ec21d73b1
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-08 20:06:13 -0700

    added route for movie search

--- server/routes/movieRoutes.ts
+++ server/routes/movieRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getPopular, getMovie, getCast } from "../controllers/movieController.js";
+import { getPopular, getMovie, getCast, searchMovie } from "../controllers/movieController.js";
 
 const movieRouter = express.Router();
 
 movieRouter.get("/getPopular/:page", getPopular);
 movieRouter.get("/getMovie/:movieId", getMovie);
 movieRouter.get("/getCast/:movieId", getCast);
+movieRouter.get("/searchMovie/:query", searchMovie);
 
 export default movieRouter;
