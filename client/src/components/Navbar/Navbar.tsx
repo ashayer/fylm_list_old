11d20beac58fa1abdf1646fc97285ca2801b49cb
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-08 20:07:01 -0700

    added search bar in navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
-import { Box, Button, Grid, IconButton, Link, Typography } from "@mui/material";
+import { Button, Grid, IconButton, Link, TextField, Typography } from "@mui/material";
 import useAuthStore from "../../stores/authStore";
 import { useNavigate } from "react-router-dom";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import styles from "./navbarStyles";
 import axios from "axios";
+import SearchIcon from "@mui/icons-material/Search";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  movieSearch: yup.string(),
+});
 
 const Navbar = () => {
   const user = useAuthStore((state) => state.isUser);
@@ -19,6 +26,16 @@ const Navbar = () => {
     await axios.post("/api/user/logout");
   };
 
+  const formik = useFormik({
+    initialValues: {
+      movieSearch: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      navigate(`/search/${values.movieSearch}`);
+    },
+  });
+
   return (
     <Grid item container sx={{ ...styles.navBarContainer }} xs={11}>
       <Grid item sx={{ flexGrow: 1 }}>
@@ -30,17 +47,39 @@ const Navbar = () => {
       </Grid>
       {user && (
         <>
+          <Grid item>
+            <form onSubmit={formik.handleSubmit}>
+              <TextField
+                fullWidth
+                id="movieSearch"
+                name="movieSearch"
+                variant="standard"
+                autoComplete="true"
+                placeholder="Search For Movie"
+                value={formik.values.movieSearch}
+                onChange={formik.handleChange}
+                InputProps={{
+                  startAdornment: (
+                    <IconButton onClick={() => formik.handleSubmit()}>
+                      <SearchIcon />
+                    </IconButton>
+                  ),
+                }}
+              />
+            </form>
+          </Grid>
           <Grid
             item
             sx={{
               ...styles.navBarIsUserBox,
             }}
+            order={{ lg: 1, xs: 2 }}
           >
             <IconButton sx={{ color: "black" }} onClick={() => navigate(`/user/${username}`)}>
               <AccountBoxIcon fontSize="large" />
             </IconButton>
           </Grid>
-          <Grid item>
+          <Grid item order={{ lg: 1, xs: 3 }}>
             <Button variant="contained" onClick={() => logoutUser()}>
               Log Out
             </Button>
