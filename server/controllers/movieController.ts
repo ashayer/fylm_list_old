996258c9c1dd7fb4ccbe9800e64f581172069732
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-08 20:06:26 -0700

    added controller function for movie search

--- server/controllers/movieController.ts
+++ server/controllers/movieController.ts
@@ -39,3 +39,16 @@ export const getCast = async (req: express.Request, res: express.Response) => {
     res.status(404).json({ err });
   }
 };
+
+export const searchMovie = async (req: express.Request, res: express.Response) => {
+  const { query } = req.params;
+  const encodedQuery = encodeURIComponent(query);
+  try {
+    const movie = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_DB_API_KEY}&query=${encodedQuery}&page=1`,
+    );
+    res.status(200).json(movie.data);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+};
